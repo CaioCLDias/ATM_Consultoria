4c1721459cf10a5682dd246bedae3f2bf45fe579
Caio Dias <caio.lorenzon.dias@gmail.com>
2021-01-13 21:30:34 -0300

    Criando a Tela de Empresa

--- lib/telaEmpresa.dart
+++ lib/telaEmpresa.dart
@@ -14,15 +14,40 @@ class _TelaEmpresaState extends State<TelaEmpresa> {
         title: Text('Robot Time'),
         backgroundColor: Colors.lightBlue,
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Row(
-
-            )
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset(
+                    'images/rt_logo.png',
+                    height: 75,
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                      'Robot Time',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text('Somos uma Garagem para desenvolvimento das habilidades do futuro, '
+                    'nossa missão é desenvolver a mentalidade e o potencial dos alunos utilizando atividades '
+                    'práticas e criativas, utilizando o material da LEGO Education e outras ferramentas.'
+                ),
+              )
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 }
